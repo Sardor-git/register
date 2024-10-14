2ce206f0e722a7610fd574f1bbfd35ddab138f81
Sardor-git <toirov.sardor.dev@gmail.com>
2024-10-14 22:29:35 +0500

    first commit

---
+++ adminapp/models.py
@@ -0,0 +1,10 @@
+from django.db import models
+
+class Faculty(models.Model):
+    name = models.CharField(max_length=100, null=False, blank=False)
+    def __str__(self):
+        return self.name
+
+
+
+
